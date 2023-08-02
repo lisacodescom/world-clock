cfce5d586b9b63302b86b90f70f764316a49264e
lisacodescom <alisalove896@gmail.com>
2023-08-01 22:52:28 -0500

    add JS

---
+++ JavaScript.js
@@ -0,0 +1,22 @@
+function city(){
+let dallasElement = document.querySelector("#dallas")
+let dallasDateElement = dallasElement.querySelector(".date")
+let dallasDate = moment().tz("America/Dallas");
+dallasDateElement.innerHTML = dallasDate.format("MMMM Do YYYY");
+let dallasTimeElement = dallasElement.querySelector(".time")
+let dallasTime = dallasDate.format("h:mm:ss [<small>]A[</small>]");
+dallasTimeElement.innerHTML = dallasTime;
+
+
+
+let parisElement = document.querySelector("#paris")
+let parisDateElement = parisElement.querySelector(".date")
+let parisDate = moment().tz("Europe/Paris");
+parisDateElement.innerHTML = parisDate.format("MMMM Do YYYY")
+let parisTimeElement = parisElement.querySelector(".time")
+let parisTime = parisDate.format("h:mm:ss [<small>]A[</small>]");
+parisTimeElement.innerHTML = parisTime;
+
+}
+setInterval(city,1000)
+
