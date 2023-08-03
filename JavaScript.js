f1a5312ab3f14685570d1fad30343117a9dcf6bc
lisacodescom <alisalove896@gmail.com>
2023-08-02 22:25:56 -0500

    add js element

--- JavaScript.js
+++ JavaScript.js
@@ -35,10 +35,9 @@ function updateCity(event) {
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format(
-          "h:mm:ss [<small>]A[</small>]"
-        )}
-    </div>`;
+        <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}
+    </div>
+  `;
 
 }
 
